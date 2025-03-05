b088e74776f472615a27b356398c4dec34fb8b12
Su Nandar Lin <hsunandar.linn98@gmail.com>
2025-03-06 01:26:21 +0800

    val param fix

--- api/db-datastore.js
+++ api/db-datastore.js
@@ -32,7 +32,6 @@ module.exports.put = (id, val) => {
 
 module.exports.post = async (id, val) => {
   const [data] = await datastore.get(key(id));
-  const tempval = data.val ? data.val : 0;
-  const updatedval = tempval + val;
-  return datastore.save({ key: key(id), data: { name: id, updatedval } });
+  const updatedval = (data && data.val ? data.val : 0) + val;
+  return datastore.save({ key: key(id), data: { name: id, val: updatedval } });
 };
