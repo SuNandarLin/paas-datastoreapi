d707c4b65f207f2cc3064a81e9e79b49661f0bfb
Su Nandar Lin <hsunandar.linn98@gmail.com>
2025-03-06 01:06:28 +0800

    fix calling correct function

--- api/index.js
+++ api/index.js
@@ -38,7 +38,7 @@ api.put("/:id(\\w+)", bodyParser.text(), async (req, res) => {
 
 api.post("/:id(\\w+)", bodyParser.text(), async (req, res) => {
   try {
-    await db.put(req.params.id, req.body);
+    await db.post(req.params.id, req.body);
     res.sendStatus(204);
   } catch (e) {
     console.error(e);
